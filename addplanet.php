480202493e74e2addb272dca6674192fef65c8c4
nuwem <juanluxerez@gmail.com>
2015-02-16 16:33:59 +0100

    fixed addplanet

--- addplanet.php
+++ addplanet.php
@@ -21,14 +21,14 @@ $query1 = 'INSERT INTO Recursos (Metal,Cristal,Deuterio)
 $query2 = 'INSERT INTO Instalaciones (Robots,Hangar,Laboratorio,Nanos)
 		   VALUES ("'.$_POST['NRobots'].'","'.$_POST['NHangar'].'","'.$_POST['NLaboratorio'].'",0)';
 $query3 = 'INSERT INTO Planetas (Nombre,TempMax,TempMin)
-		   VALUES ("'.$_POST['NNombre'].'")'.$_POST['TempMax'].'","'.$_POST['TempMin'].'")';
+		   VALUES ("'.$_POST['NNombre'].'","'.$_POST['TempMax'].'","'.$_POST['TempMin'].'")';
 $query4 = 'INSERT INTO FlotaDef (Lanzamisiles,LaseresP,LaseresG,Gauss,Ionico,Plasma,CupulaP,CupulaG,MisilesB,Satelites)
 		   VALUES (0,0,0,0,0,0,0,0,0,0)';
 		   
-		   if (!mysqli_query($link,$query1)){printf("Connect failed:");};
-		   if (!mysqli_query($link,$query2)){printf("Connect failed:");};
-		   if (!mysqli_query($link,$query3)){printf("Connect failed:");};
-		   if (!mysqli_query($link,$query4)){printf("Connect failed:");};
+		   if (!mysqli_query($link,$query3)){printf("Connect failed:3");};
+		   if (!mysqli_query($link,$query1)){printf("Connect failed:1");};
+		   if (!mysqli_query($link,$query2)){printf("Connect failed:2");};
+		   if (!mysqli_query($link,$query4)){printf("Connect failed:4");};
 		   
 		   echo '<div class="alert alert-success" role="alert"><strong>Éxito!</strong> Añadido planeta nuevo.</div>';
 ?>
